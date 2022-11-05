089d6143428d87a04cb8cb1a035cd8423e0979a6
eric polley <ericpolley@live.com>
2022-11-05 07:50:36 -0700

    button console logs text input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import react from 'react';
+import React, { useState } from 'react';
+import './App.css'
 
 
 
 
 function App() {
+    const [newItem, setNewItem] = useState("");
 
-
-  
+    function addItem() {
+      console.log(newItem)
+    }
   return (
     <>
-      <p>blank</p>
+      <h1>Todo App</h1>
+      <input type="text" placeholder='Add Item...' value={newItem} onChange={e => setNewItem(e.target.value)} />
+      <button onClick={() => addItem()}>Add</button>
+
+      <ul>
+        <li>list a</li>
+        <li>list b</li>
+        <li>list c</li>
+      </ul>
+
     </>
   );
 }
